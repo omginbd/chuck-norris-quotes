0c3d8f11db10c71d4d96597a53a2872a045d7eb6
Michael Collier <colliermichaelp@gmail.com>
2017-02-09 04:49:06 +0000

    Add images and sound

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 $(() => {
-  console.log('heloooooo')
+	const audioElement = document.createElement('audio')
+	audioElement.setAttribute('src', 'http://soundbible.com/grab.php?id=996&type=mp3')
   const quoteHolder = $('#quote')
   const button = $('#getQuoteButton')
+	const body = document.getElementsByTagName('body')[0]
 
   button.click(() => {
     $.getJSON(
 			'http://api.icndb.com/jokes/random',
       ({value: {joke}}) => {
+				body.style.backgroundImage = getRandomImage()
+				audioElement.play()
 				quoteHolder.text(`${joke}`)
     })
   })
 })
+
+function getRandomImage() {
+	const i = Math.floor((Math.random() * 100) % 6)
+	return `url(img/${i}.jpg)`
+}
